4656b72e856347a92539d26dddb25d8c783205f8
DESKTOP-TAT098A\Admin <lamtu199513@gmail.com>
2022-12-28 20:28:14 +0700

    create spring boot project

---
+++ Assigment_1/src/main/java/com/java/assigment/Assigment1Application.java
@@ -0,0 +1,13 @@
+package com.java.assigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assigment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assigment1Application.class, args);
+    }
+
+}
---
+++ Assigment_1/src/test/java/com/java/assigment/Assigment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.assigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assigment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
